eafb3f20ad3baa78679592f056125eefa3eb047f
FreMaNgo <boping2010@gmail.com>
2019-05-08 16:02:12 +0800

    更新@plume/app,@plume/lib 的依赖

--- packages/cli/bin/dev.js
+++ packages/cli/bin/dev.js
@@ -2,7 +2,7 @@
 const App = require("@plume/app");
 const Lib = require("@plume/lib");
 
-module.exports = (customConfig, mode) => {
-  const instance = mode === "app" ? new App(customConfig) : new Lib(customConfig);
+module.exports = (config, mode) => {
+  const instance = mode === "app" ? new App(config) : new Lib(config);
   instance.dev();
 };
